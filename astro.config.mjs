84b9cd2dbf6d86e63ea0da6bb520ace34e6df508
starrlord1 <21917365-starrlord1@users.noreply.gitlab.com>
2024-07-06 00:33:16 -0400

    config site updated to match github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://techmurda.eth',
+    site: 'starrlord1.github.io',
 });
