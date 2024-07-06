ddea8dbe37f5b9ac1b35a9099b5506df7caf07fa
starrlord1 <21917365-starrlord1@users.noreply.gitlab.com>
2024-07-06 17:20:06 -0400

    removed imports to test functionality via tma

--- src/telegram/App.tsx
+++ src/telegram/App.tsx
@@ -1,14 +1,6 @@
 import WebApp from "@twa-dev/sdk";
 
 import BaseLayout from "../Layouts/BaseLayout.astro";
-import Index from "../pages/index.astro";
-import About from "../pages/about.astro";
-import Blog from "../pages/blog.astro";
-import Topics from "../pages/Topics.astro";
-import BlogPost from "../components/BlogPost.astro";
-import Footer from "../components/Footer.astro";
-import Hamburger from "../components/Hamburger.astro";
-import Header from "../components/Header.astro";
-import Navigation from "../components/Navigation.astro";
-import Social from "../components/Social.astro";
-import ThemeIcon from "../components/ThemeIcon.astro";
+
+
+
